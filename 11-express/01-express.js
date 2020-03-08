452fc6f72a47ed1632f78480017327ec8fc0a225
phoenixfate <sm516116978@outlook.com>
2020-03-08 20:41:15 +0800

    express 中间件；body-parser

--- 11-express/01-express.js
+++ 11-express/01-express.js
@@ -7,6 +7,75 @@ var app=new express()
 //但需要配置ejs模板引擎
 app.set("view engine","ejs")
 
+//中间件app.use
+//express内置中间件
+//使用express配置静态资源路径
+//默认是public路径
+app.use(express.static("public"))
+//配置虚拟目录的静态资源路径
+// app.use('/static',express.static("public"))
+
+
+
+
+/**
+ * body-parser 中间件 第三方的 获取post提交的数据
+ * 1.cnpm install body-parser --save
+ * 2.var bodyParser=require('body-parser')
+ * 3.设置中间件
+ */
+const bodyParser=require("body-parser")
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/login",function(request,response){
+    response.render("login")
+})
+
+app.post('/doLogin',function(request,response){
+    //获取post提交的数据
+    //4.request.body获取post提交的数据
+    console.log(request.body)
+    response.send("登陆成功")
+})
+
+
+/**
+ * node中间件
+ * 应用级中间件
+ */
+//匹配任意路径
+app.use(function(request,response,next){
+    console.log(new Date())
+    //默认匹配到路由之后不向下匹配
+    //使路由继续向下匹配
+    next()
+})
+
+//匹配指定路由
+app.use('/temp',function(request,response,next){
+    console.log("temp 应用级别中间件")
+    //默认匹配到路由之后不向下匹配
+    //使路由继续向下匹配
+    next()
+})
+
+
+/**
+ * 路由级中间件
+ */
+app.get("/temp",function(request,response,next){
+    console.log("temp 路由中间件")
+    next()
+})
+
+app.get("/temp",function(request,response){
+    response.send("temp 最终响应数据")
+})
+
 
 app.get("/",function(request,response){
     response.send("你好 express")
@@ -46,12 +115,12 @@ app.get("/index",(request,response)=>{
     })
 })
 
-//中间件app.use
-//使用express配置静态资源路径
-//默认是public路径
-app.use(express.static("public"))
-//配置虚拟目录的静态资源路径
-// app.use('/static',express.static("public"))
+
+
+//上面的路由都没有匹配到，则返回404
+app.use(function(request,response){
+    response.status(404).send("没有匹配到页面，返回404")
+})
 
 
 
